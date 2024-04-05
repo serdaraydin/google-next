25d56a7d9e8b4b3c0ab8d4dcb2658cc4bd59c6db
serdaraydin <serdara2000@gmail.com>
2024-04-05 23:25:05 +0300

    Add Body Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+          alt="Goole Logo"
+          width={300}
+          height={100}
+          priority
+          style={{ width: "auto" }}
+        />
+        <HomeSearch />
+      </div>
     </>
   );
 }
